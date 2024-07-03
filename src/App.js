c8f1a60a531dbb5a16ba62c6fb561b1fb265fff2
Alexander <alexanderi.webdev@gmail.com>
2024-07-03 17:45:17 +0400

    lesson 11

--- src/App.js
+++ src/App.js
@@ -7,25 +7,49 @@ import Cart from "./components/Cart";
 import Home from "./Pages/Home";
 import NotFound from "./components/NotFound";
 
+import { RootState } from "./Redux/Store";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./Redux/counterSlice";
+
 export const SearchContext = React.createContext();
 
 function App() {
   const [inputValue, setInputValue] = React.useState("");
 
-  return (
-    <SearchContext.Provider value={{ inputValue, setInputValue }}>
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+  return (
+    // <SearchContext.Provider value={{ inputValue, setInputValue }}>
+    //   <div className="wrapper">
+    //     <Header />
+    //     <div className="content">
+    //       <Routes>
+    //         <Route path="/" element={<Home />} />
+    //         <Route path="/cart" element={<Cart />} />
+
+    //         <Route path="*" element={<NotFound />} />
+    //       </Routes>
+    //     </div>
+    //   </div>
+    // </SearchContext.Provider>
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
       </div>
-    </SearchContext.Provider>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {  BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Redux/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+  <Provider store={store}>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
-
+  </Provider>
 );
