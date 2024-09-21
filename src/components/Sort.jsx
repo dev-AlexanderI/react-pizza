e9ee7f84380b331d01d8549cf85699f59d3c0603
Alexander <alexanderi.webdev@gmail.com>
2024-09-21 12:52:38 +0400

    lesson 16

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setSort } from "../Redux/slices/filterSlice";
 export const sortNames = [
@@ -14,13 +14,26 @@ function Sort() {
   const sort = useSelector((state) => state.filter.sort);
   const [open, setOpen] = React.useState(false);
 
+  const sortRef = React.useRef();
+
   function onClickListSort(obj) {
     dispatch(setSort(obj));
     setOpen(false);
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.composedPath().includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+    document.body.addEventListener("click", handleClickOutside);
+    return () => {
+      document.body.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
   return (
-    <div className="sort">
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         <svg
           width="10"
